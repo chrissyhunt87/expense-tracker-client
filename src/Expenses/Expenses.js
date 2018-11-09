f77a629c8d0454edd0ca3ab94118028bc9a37baa
Chrissy Hunt <chrissy.hunt@gmail.com>
2018-11-09 10:53:31 -0500

    Set up basic Home, Login, Expenses parent components

---
+++ src/Expenses/Expenses.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Expenses extends Component {
+    render() {
+        return (
+            <p>Expenses component</p>
+        )
+    }
+}
+
+export default Expenses;
---
+++ src/Login/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    render() {
+        return (
+            <p>Login component</p>
+        )
+    }
+
+}
+
+export default Login;
