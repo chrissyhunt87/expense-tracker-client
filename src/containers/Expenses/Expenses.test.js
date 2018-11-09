54ac338f5ee906220b2b903a175854e597cb2f57
Chrissy Hunt <chrissy.hunt@gmail.com>
2018-11-09 16:59:14 -0500

    Add text test to Expenses.test.js

--- src/containers/Expenses/Expenses.test.js
+++ src/containers/Expenses/Expenses.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
+import { shallow, render } from 'enzyme';
 import Expenses from './Expenses';
 
 describe('Expenses component', () => {
@@ -13,5 +13,10 @@ describe('Expenses component', () => {
     it('should render correctly in "debug" mode', () => {
         const component = shallow(<Expenses debug />);
         expect(component).toMatchSnapshot();
+    })
+
+    it('should show correct header text', () => {
+        const wrapper = shallow(<Expenses />);
+        expect(wrapper.find('h1').text("Hello, Chrissy!"));
     })
 });
